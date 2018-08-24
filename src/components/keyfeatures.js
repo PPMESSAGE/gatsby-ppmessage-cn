7cabb0340203f4841328e67eea0ca7336f6a269f
Guijin Ding <dingguijin@gmail.com>
2018-08-24 14:21:48 +0800

    test key features

---
+++ src/components/keyfeatures.js
@@ -0,0 +1,72 @@
+import React from 'react'
+
+import "./keyfeatures.css"
+
+// {abstract: {icon:, title:, desc}}
+// children is modal detail
+
+class KeyFeatures extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+
+        this.clickFeature = this.clickFeature.bind(this);
+    }
+
+    clickFeature(feature, e) {
+        console.log("clickFeature", feature);
+        this.setState({highlight_item: feature.feature_id});
+    }
+
+    
+    render() {
+
+        this.props.features.map((feature, i) => {
+            feature.feature_id = i;
+            return feature;
+        });
+        
+        let feature_desc_list = this.props.features.map((feature, i)=> {
+            let _class = "";
+
+            if (this.state.highlight_item == i) {
+                _class = "active";
+            }
+
+            return (
+                <div onClick={this.clickFeature.bind(this, feature)} className={_class} key={i} value={i}>
+                    <div>{feature.title}</div>
+                    <div>{feature.desc}</div>
+                </div>
+            )
+        })
+
+        let feature_image_list = this.props.features.map((feature, i)=> {
+            let _class = "feature-image";
+            if (this.state.highlight_item == i) {
+                _class = "feature-image active";
+            }
+
+            return (
+                <div className={_class} key={i}>
+                    <img src={feature.image} />
+                </div>
+            )
+        })
+
+        return (
+            <div className="key-features">
+                <div className="key-features-left">
+                    {feature_desc_list}
+                </div>
+                <div className="key-features-right">
+                    {feature_image_list}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default KeyFeatures
---
+++ src/pages/test/test-key-features.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+import styles from "../about-css-modules.module.css";
+import KeyFeatures from "../../components/keyfeatures"
+
+
+export default () => {
+    let _features = [
+        {
+            title: "Segmentation",
+            desc: "Create live segments and target the right people for messages based on their behaviors",
+            image: "https://marketing.intercomassets.com/assets/messages/segmentation-53f2b095305527a75a4082d62ae823fc29e4a09a3c34e1facc147b2046d1ec49.png"
+        },
+        {
+            title: "Outbound messaging",
+            desc: "Send one-off and automated email, push, and in-app messages to people",
+            image: "https://marketing.intercomassets.com/assets/messages/outbound-dbf7ecfa4c3664f7174deb855194d0061d0f2112ddf8cc0243c6438c6880cb7c.png"
+        },
+
+        {
+            title: "Measurement and Optimization",
+            desc: "See the performance of messages and test variants to improve results",
+            image: "https://marketing.intercomassets.com/assets/messages/measurement-b13614c1ef0feae4aa3509d1fe5ee4b10fc8a489f10fa8ee9a02ca3a1ca93545.png"
+        },
+
+        {
+            title: "Integrations",
+            desc: "Connect Intercom with other tools like Salesforce, Segment, and more",
+            image: "https://marketing.intercomassets.com/assets/messages/integrations-c420a951ff2bb5ce602e9149567455760bea13a8f8246701593b577238096f75.png"
+        },
+        
+
+    ]
+
+    return (
+        <div>
+            <div style={{height:`72px`}}></div>
+            <div className={styles.container__narrower}>
+                <div className={styles.g__row}>
+                    <div className={styles.g__c6}>
+                        <h1>test key features</h1>
+                        <KeyFeatures features={_features}>
+                            
+                        </KeyFeatures>
+                    </div>
+                </div>  
+            </div>
+        </div>
+    )
+
+}
