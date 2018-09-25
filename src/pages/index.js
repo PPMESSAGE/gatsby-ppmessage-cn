828f102cbd0120ebbcf2ea4199e83bd65db31317
Guijin Ding <dingguijin@gmail.com>
2018-09-25 17:22:24 +0800

    worth company

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./about-css-modules.module.css";
 import EnterYourMobile from "../components/enteryourmobile";
 import KeyFeatures from "../components/keyfeatures";
+import WorthCompany from "../components/worthcompany";
 import Layout from "./layout";
 
 
@@ -166,109 +167,7 @@ export default () =>{
                 </div>  
             </div>
             <section>
-                <div className={styles.f__center}>
-                    <h3 className={styles.f__h3}>
-                        <font style={{color:"#888",fontSize:"24px",fontWeight:"300"}}>值得全球最具创新精神的企业信赖-无论大小
-                        </font>
-                    </h3>
-                    <div style={{height:'20px'}}></div>
-                    <div className={styles.logo_container}>
-                        <div className={styles.index_custom_logo}>
-                            <div className={styles.row_logo}>
-
-                                <div className={styles.col_2}>
-                                    <div className={styles.index_custom_logo_div}>
-                                        <span className={styles.index_custom_1}>
-                                        </span>
-                                    </div>
-                                </div>
-
-                                <div className={styles.col_2}>
-                                    <div className={styles.index_custom_logo_div}>
-                                        <span className={styles.index_custom_2}>
-                                        </span>
-                                    </div>
-                                </div>
-
-                                <div className={styles.col_2}>
-                                    <div className={styles.index_custom_logo_div}>
-                                        <span className={styles.index_custom_3}>
-                                        </span>
-                                    </div>
-                                </div>
-
-                                <div className={styles.col_2}>
-                                    <div className={styles.index_custom_logo_div}>
-                                        <span className={styles.index_custom_4}>
-                                        </span>
-                                    </div>
-                                </div>
-
-                                <div className={styles.col_2}>
-                                    <div className={styles.index_custom_logo_div}>
-                                        <span className={styles.index_custom_5}>
-                                        </span>
-                                    </div>
-                                </div>
-
-                                <div className={styles.col_2}>
-                                    <div className={styles.index_custom_logo_div}>
-                                        <span className={styles.index_custom_6}>
-                                        </span>
-                                    </div>
-                                </div>                            
-                            </div>
-                            <div className={styles.row_logo}>
-
-                                <div className={styles.col_2}>
-                                    <div className={styles.index_custom_logo_div}>
-                                        <span className={styles.index_custom_7}>
-                                        </span>
-                                    </div>
-                                </div>
-
-                                <div className={styles.col_2}>
-                                    <div className={styles.index_custom_logo_div}>
-                                        <span className={styles.index_custom_8}>
-                                        </span>
-                                    </div>
-                                </div>
-
-                                <div className={styles.col_2}>
-                                    <div className={styles.index_custom_logo_div}>
-                                        <span className={styles.index_custom_9}>
-                                        </span>
-                                    </div>
-                                </div>
-
-                                <div className={styles.col_2}>
-                                    <div className={styles.index_custom_logo_div}>
-                                        <span className={styles.index_custom_10}>
-                                        </span>
-                                    </div>
-                                </div>
-
-                                <div className={styles.col_2}>
-                                    <div className={styles.index_custom_logo_div}>
-                                        <span className={styles.index_custom_11}>
-                                        </span>
-                                    </div>
-                                </div>
-
-                                <div className={styles.col_2}>
-                                    <div className={styles.index_custom_logo_div}>
-                                        <span className={styles.index_custom_12}>
-                                        </span>
-                                    </div>
-                                </div>
-
-                                
-                                
-                            </div>
-
-                        </div>
-                    </div>
-                </div>            
+                <WorthCompany />
             </section>
             <section>
                 <div className={styles.index_200px}></div>
