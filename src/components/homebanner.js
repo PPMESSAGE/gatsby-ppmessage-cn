3bd9e10a424a00888c33fd71d81c3849a2c882c7
Guijin Ding <dingguijin@gmail.com>
2018-09-26 11:17:38 +0800

    home banner no emoji

--- src/components/homebanner.js
+++ src/components/homebanner.js
@@ -3,7 +3,7 @@ import React from "react"
 import "./homebanner.css"
 
 export default () => (
-    <div className="home-banner">🧐
+    <div className="home-banner">
         <a className="link__arrow" href="/ppmessage-upgrade-notes">
             重大升级，收费策略更改，赶紧看看...
         </a>
